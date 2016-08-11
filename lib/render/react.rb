06b94ad71a6a346995b9795676ac465c819a8bbc
Alexander Krasnoschekov <akrasnoschekov@gmail.com>
2016-08-11 17:18:21 +0300

    adds more logic

--- lib/render/react.rb
+++ lib/render/react.rb
@@ -1,6 +1,7 @@
 require 'render/react/version'
 require 'v8'
 require 'json'
+require 'active_support/inflector'
 
 module Render
   module React
@@ -9,21 +10,20 @@ module Render
       @react_render_cxt = V8::Context.new
       @react_render_cxt.load(js_path + '/react/dist/react.min.js')
       @react_render_cxt.load(js_path + '/react-dom/dist/react-dom-server.min.js')
+
+      components_path = File.expand_path('../../../js/components', __FILE__)
+      @react_render_lookup = {}
+      Dir.glob(File.join(components_path, '**', '*.js')).each do |filename|
+        @react_render_cxt.load(filename)
+        @react_render_lookup[File.basename(filename, '.js').camelize.to_sym] = true
+      end
+
       super
     end
 
-    def render_react_eval_js(js)
-      @react_render_cxt.eval(js)
-    end
-
-    def react(_component_class, js_file_path, **props)
-      code = <<-EOF
-        #{File.read(js_file_path)}
-
-        var element = React.createElement(HelloMessage, #{JSON.dump(props)});
-        ReactDOMServer.renderToString(element);
-      EOF
-      render_react_eval_js(code)
+    def react(component_class, **props)
+      raise "#{component_class} not found in ..." unless @react_render_lookup[component_class.to_sym]
+      @react_render_cxt.eval "ReactDOMServer.renderToString(React.createElement(#{component_class}, #{JSON.dump(props)}));"
     end
   end
 end
--- spec/render/react_spec.rb
+++ spec/render/react_spec.rb
@@ -3,18 +3,50 @@ require 'spec_helper'
 describe Render::React do
   subject { Class.new.include(Render::React).new }
 
-  it '#render_react_eval_js evals plain js' do
-    expect(subject.render_react_eval_js('2*2')).to eq(4)
+  it 'evals plain js' do
+    expect(
+      subject.send(:instance_variable_get, :@react_render_cxt).eval('2*2')
+    ).to eq(4)
   end
 
-  it '#react renders simple component' do
-    name = "Frank"
+  it 'renders simple component' do
+    name = 'Frank'
     output = subject.react(
       :HelloMessage,
-      "#{FIXTURES_PATH}/component.js",
       name: name
     )
-    expect(output).to include("data-reactid")
+    expect(output).to include('data-reactid')
     expect(output).to include(name)
+  end
+
+  xit 'doesn\'t have memory leak' do
+    samples1 = []
+    10.times do |_i|
+      100.times do |_j|
+        output = subject.react(
+          :HelloMessage,
+          name: :LeakyName
+        )
+      end
+      pid, size = `ps ax -o pid,rss | grep -E "^[[:space:]]*#{$PROCESS_ID}"`.strip.split.map(&:to_i)
+      samples1 << size
+      GC.start
+    end
+    puts "RSS #{samples1.inject(:+).to_f / samples1.size}"
+
+    samples1 = []
+    10.times do |_i|
+      100.times do |_j|
+        output = subject.react(
+          :HelloMessage,
+          name: :LeakyName
+        )
+      end
+      pid, size = `ps ax -o pid,rss | grep -E "^[[:space:]]*#{$PROCESS_ID}"`.strip.split.map(&:to_i)
+      samples1 << size
+      GC.start
+    end
+    puts "RSS #{samples1.inject(:+).to_f / samples1.size}"
+    expect(true).to be_true
   end
 end
